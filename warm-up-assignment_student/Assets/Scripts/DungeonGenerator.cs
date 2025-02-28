e9829d2414ab229b299b393bf8f2ba98e0b97581
RaoulHeurneman <raoul.heurneman@gmail.com>
2025-02-28 15:03:20 +0100

    dungeon generates!

--- warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
+++ warm-up-assignment_student/Assets/Scripts/DungeonGenerator.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using NaughtyAttributes;
+using System;
 using UnityEngine;
 
 
@@ -6,44 +8,77 @@ public class DungeonGenerator : MonoBehaviour
 {
     [SerializeField] private RectInt dungeonBounds = new RectInt(0, 0, 100, 50);
     [SerializeField] private int minRoomSize = 5;
-    [SerializeField] private int maxRoomSize = 10;
-    [SerializeField] private bool splitHorizontally;
-    [SerializeField] private List<RectInt> rooms = new List<RectInt>();
-    
+    [SerializeField] private int wallHeight;
+    private bool splitHorizontally;
+    private List<RectInt> toDoRooms = new List<RectInt>();
+    private List<RectInt> doneRooms = new List<RectInt>();
 
+    System.Diagnostics.Stopwatch stopwatch;
+
+    
+    [Button("Generate")]
     void Start()
     {
-        rooms.Add(dungeonBounds);
-        Splitrooms(dungeonBounds);
+        stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        doneRooms.Clear();
+        toDoRooms.Add(dungeonBounds);
+        for (int i = 0; i < toDoRooms.Count; i++)
+            Splitrooms(toDoRooms[i]);
+
+        toDoRooms.Clear();
+        stopwatch.Stop();
+        Debug.Log(Math.Round(stopwatch.Elapsed.TotalMilliseconds, 3));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        foreach (RectInt room in rooms)
+        foreach (RectInt room in doneRooms)
         {
-            AlgorithmsUtils.DebugRectInt(room, Color.yellow);
+            AlgorithmsUtils.DebugRectInt(room, Color.yellow,0, false, wallHeight);
         }
     }
+
+    #region Spliting
     void Splitrooms(RectInt room)
     {
+        if (room.width > room.height)
+            splitHorizontally = false;
+        else
+            splitHorizontally = true;
+
         if (splitHorizontally)
         {
-            int split = Random.Range(minRoomSize, room.height - minRoomSize);
+            int split = UnityEngine.Random.Range(minRoomSize, room.height - minRoomSize);
             RectInt newRoom1 = new RectInt(room.x, room.y, room.width, split);
-            rooms.Add(newRoom1);
             RectInt newRoom2 = new RectInt(room.x, room.y + split, room.width, room.height - split);
-            rooms.Remove(room);
-            rooms.Add(newRoom2);
+
+            if (newRoom1.height >= minRoomSize * 2 && newRoom1.width >= minRoomSize * 2)
+                toDoRooms.Add(newRoom1);
+            else
+                doneRooms.Add(newRoom1);
+
+            if (newRoom2.width >= minRoomSize * 2 && newRoom2.height >= minRoomSize * 2)
+                toDoRooms.Add(newRoom2);  
+            else
+                doneRooms.Add(newRoom2);
         }
+
         else
         {
-            int split = Random.Range(minRoomSize, room.width - minRoomSize);
+            int split = UnityEngine.Random.Range(minRoomSize, room.width - minRoomSize);
             RectInt newRoom1 = new RectInt(room.x, room.y, split, room.height);
-            rooms.Add(newRoom1);
             RectInt newRoom2 = new RectInt(room.x + split, room.y, room.width - split, room.height);
-            rooms.Remove(room);
-            rooms.Add(newRoom2);
+
+            if (newRoom1.height >= minRoomSize * 2 && newRoom1.width >= minRoomSize * 2)
+                toDoRooms.Add(newRoom1);
+            else
+                doneRooms.Add(newRoom1);
+
+            if (newRoom2.width >= minRoomSize * 2 && newRoom2.height >= minRoomSize * 2)
+                toDoRooms.Add(newRoom2);  
+            else
+                doneRooms.Add(newRoom2); 
         }
     }
+    #endregion 
 }
